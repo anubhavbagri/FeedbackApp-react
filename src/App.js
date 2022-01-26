c57a87f282623729bec621e6f2b19e7dc96f4221
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-26 21:56:27 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comm, indx) => (
-          <li key={indx}>{comm.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-
-      {/* {Math.random() * (5 + 5)} */}
+      <h1>My App</h1>
     </div>
   );
 }
