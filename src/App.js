cca50ed046b3cb66abdeec0c132fe745f0bcb46e
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-26 21:24:26 +0530

    feat: Dynamic values & Lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 function App() {
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
+
   return (
     <div className="container">
-      <h1>My App</h1>
-      <p>Hello</p>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comm, indx) => (
+            <li key={indx}>{comm.text}</li>
+          ))}
+        </ul>
+      </div>
+      {/* {Math.random() * (5 + 5)} */}
     </div>
   );
 }
