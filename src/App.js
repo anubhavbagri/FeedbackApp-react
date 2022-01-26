2852b74007ecc060c3df8993313917b1715f90b3
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-26 21:08:52 +0530

    feat: without using JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+
 function App() {
-  return <h1>Hello from the app component</h1>;
+  return React.createElement(
+    'div',
+    { className: 'container' },
+    // {} i.e. no attributes
+    React.createElement('h1', {}, 'My App')
+  );
 }
 
 export default App;
