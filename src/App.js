0e39d8f1075d5e9a3bcfc53a495593520fecdef9
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-26 21:55:22 +0530

    feat: Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ function App() {
     { id: 3, text: 'Comment three' },
   ];
 
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comm, indx) => (
+          <li key={indx}>{comm.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comm, indx) => (
-            <li key={indx}>{comm.text}</li>
-          ))}
-        </ul>
-      </div>
+
+      {showComments && commentBlock}
+
       {/* {Math.random() * (5 + 5)} */}
     </div>
   );
