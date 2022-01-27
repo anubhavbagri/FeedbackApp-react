7aafa79a8dbef21ca93ff2c70f45da947d0ee818
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-27 06:17:37 +0530

    feat: Managing global state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,10 @@
 // Component that shows a single feedback item which will include the rating & the text
 
-import { useState } from 'react';
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState('This is an example of a feedback item');
-
-  const handleClick = () => {
-    setRating((prev) => {
-      console.log(prev); // using previous value
-      return prev + 1;
-    });
-  };
-
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   );
 }
