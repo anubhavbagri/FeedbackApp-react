5a4c405c7b32d2644a0ebd4704e67d9178b8fe35
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-28 18:14:16 +0530

    feat: creating styled component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,12 @@
 // Component that shows a single feedback item which will include the rating & the text
+import Card from './shared/Card';
 
 function FeedbackItem({ item }) {
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
 
