447850be7efbc24a7102b7126c7428cf21725bbb
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-28 19:16:31 +0530

    feat: add propTypes

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,18 @@
 // Component that shows a single feedback item which will include the rating & the text
+import PropTypes from 'prop-types';
 import Card from './shared/Card';
 
 function FeedbackItem({ item }) {
   return (
-    <Card reverse={true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
