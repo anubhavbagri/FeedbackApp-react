b46ceaa3026a977b4382949c44f89afb15536a99
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-30 00:38:39 +0530

    feat: `useNavigate` for redirecting

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,15 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Post() {
-  const status = 404;
+  const status = 200;
+
+  const navi = useNavigate();
+
+  const clickHandler = () => {
+    console.log('Hello');
+    // if you ever wanna have to redirect after maybe updating something on the server & then wanna go to a new page
+    navi('/about');
+  };
 
   if (status === 404) {
     return <Navigate to="/notfound" />;
@@ -10,6 +18,7 @@ function Post() {
   return (
     <div>
       <h1>Post</h1>
+      <button onClick={clickHandler}>Click</button>
     </div>
   );
 }
