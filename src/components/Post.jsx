1893099554fecdee054bc8e08510b0f81a8eeec5
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-30 00:34:26 +0530

    feat: `Navigate` of react router

---
+++ src/components/Post.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+
+function Post() {
+  const status = 404;
+
+  if (status === 404) {
+    return <Navigate to="/notfound" />;
+  }
+
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+}
+
+export default Post;
