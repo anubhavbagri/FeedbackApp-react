f7573539aa22efd881dd8e6f08d1a5a44f9989f9
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-01-30 03:06:44 +0530

    feat: move addFeedback func to context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
@@ -11,15 +8,6 @@ import { FeedbackProvider } from './context/FeedbackContext';
 import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    // console.log(newFeedback);
-    setFeedback([newFeedback, ...feedback]);
-    // it creates a new array with old feedback and new one
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -31,7 +19,7 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackForm />
                   <FeedbackStats />
                   <FeedbackList />
                 </>
